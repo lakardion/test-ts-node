861b52ff56208422376e5875e17485439924e103
lakardion <lakardion@protonmail.com>
2023-09-13 09:50:39 -0300

    chore: updates to repo - add ts-node as dev dep - add dotenv to use env variables - move to pnpm - Fix issue in tsconfig with module and module resolution - upgrade ts to latest - fix command not requiring index

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+import "dotenv/config";
+
 import { anotherTestFunction } from "./src/services/another-test-service.js";
 import { testFunction } from "./src/services/test-service.js";
 
 testFunction("Hello world from ts-node");
 console.log(anotherTestFunction());
+console.log(process.env["MY_ENV_VAR"]);
